02a8be1e531a009cbc78bc1f46b3faee98bbd1d6
ST10215069_Nkosinathi Ngozo <104794600+ST10215069-Nkosinathi-Ngozo@users.noreply.github.com>
2023-04-25 23:10:23 +0200

    Add files via upload

---
+++ ice task lambda_17-04-23/Even.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ice_task_lambda_17_04_23
+{
+    public delegate List<int> sort(List<int> numbers);
+    public delegate Queue<int> even(Queue<int> digits);
+    internal class Even
+    {
+
+
+        public List<int> getEvenNumbers(List<int> numbers)
+        {
+
+            numbers.ForEach(number => { if (number % 2 == 0) Console.WriteLine(number); });
+
+
+            for (int i = 0; i < numbers.Count; i++) {
+                
+                    if (numbers[i] % 2 == 0)
+                    {
+                        Console.WriteLine(numbers[i]);
+                    }
+                
+            }
+            return numbers;
+        }
+        
+        public Queue<int> EvenNumbers(Queue<int> digits)
+        {
+            foreach(int i in digits.Where(number =>number % 2 == 0)) { 
+                Console.WriteLine($"{i}");
+            }
+
+            return digits;
+        }
+        
+    }
+}
---
+++ ice task lambda_17-04-23/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace ice_task_lambda_17_04_23
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Even e = new Even();
+            
+            List<int> numbers = new List<int>()
+            {
+                1, 7, 2, 5, 10, 16, 20, 24
+            };
+
+            Queue<int> digits = new Queue<int>();
+
+            foreach(int i in numbers)
+            {
+                digits.Enqueue(i);
+            }
+
+            sort s = e.getEvenNumbers;
+            List<int> list = s(numbers);
+
+            even ev = e.EvenNumbers;
+            Queue<int> q = ev(digits);
+
+            int x = numbers[2];
+            int y = numbers[3];
+
+
+            Func<int, int, int > add = delegate { return x + y; };
+            Func<int, int, int> subtract = delegate { return x- y; };
+            Func<int, int, int> multiply = delegate { return x * y; };
+            Func<int, int, int> divide = delegate { return x / y; };
+
+            Console.WriteLine($"x + y = {add(x, y)}");
+            Console.WriteLine($"x - y = {subtract(x, y)}");
+            Console.WriteLine($"x * y = {multiply(x, y)}");
+            Console.WriteLine($"x / y = {divide(x, y)}");
+        }
+    }
+}
